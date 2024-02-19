79785a218acb2af3f56663c4ba05e8e8951b4460
ima_wizzard <aitor.vannevel@gmail.com>
2024-02-19 16:19:17 +0100

    added visualisation for multiple ledstrips

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -1,19 +1,37 @@
 <template>
   <main>
     <div>
+      <label for="numberOfStrips">Aantal LED-strips:</label>
+      <input type="number" id="numberOfStrips" v-model.number="numberOfStrips" />
+    </div>
+
+    <template v-for="stripIndex in numberOfStrips" :key="stripIndex">
+      <div>
+        <h3>LED-strip {{ stripIndex }}</h3>
+        <div v-if="colors[stripIndex - 1]?.length > 0">
+          <div
+            v-for="(color, index) in colors[stripIndex - 1]"
+            :key="index"
+            :style="{
+              backgroundColor: color,
+              width: '35px',
+              height: '15px',
+              display: 'inline-block',
+              margin: '5px',
+            }"
+          ></div>
+        </div>
+      </div>
+    </template>
+
+    <div>
       <label for="brightness">Helderheid: </label>
-      <input type="range" id="brightness" min="0" max="255" v-model="brightness">
+      <input type="range" id="brightness" min="0" max="255" v-model="brightness" />
     </div>
 
     <div>
       <label for="colorPicker">Kies een kleur:</label>
-      <input type="color" id="colorPicker" v-model="selectedColor">
-    </div>
-
-    <div v-if="colors.length > 0">
-      <div v-for="(color, index) in colors" :key="index" :style="{ display: 'inline-block' }" >
-        <div v-if="index %5==0" :key="index" :style="{ backgroundColor: color, width: '35px', height: '15px', display: 'inline-block', margin: '5px' }"></div>
-      </div>
+      <input type="color" id="colorPicker" v-model="selectedColor" />
     </div>
 
     <div>
@@ -22,6 +40,7 @@
         <option value="0">Beide strips</option>
         <option value="1">Enkel de eerste strip</option>
         <option value="2">Enkel de tweede strip</option>
+        <!-- Hier kun je extra opties toevoegen voor extra strips als dat nodig is -->
       </select>
     </div>
   </main>
@@ -34,47 +53,53 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      numberOfStrips: 1,
       brightness: 200,
       selectedColor: '#ff0000',
       selectedStrip: 0,
-      colors: []
+      colors: [], // This should be defined in your data object
     };
   },
   methods: {
     fetchColors() {
-      axios.get('http://ic2.local/json/live')
-        .then(response => {
-          // Assuming response.data is an array of color values (e.g., ["#FF0000", "#00FF00", "#0000FF"])
-          this.colors = response.data;
-          const colorsObject = response.data.leds; // Haal het object met kleursleutel-waardeparen op
-          this.colors = Object.values(colorsObject).map(color => `#${color}`);
-        })
-        .catch(error => {
-          console.error('Error fetching colors:', error);
+      for (let i = 1; i <= this.numberOfStrips; i++) {
+        axios
+          .get('http://ic' + i + '.local/json/live')
+          .then((response) => {
+            const colorsObject = response.data.leds;
+            const colors = Object.values(colorsObject).map((color) => `#${color}`);
+            // Set the colors for the corresponding strip index
+            this.colors[i - 1] = colors;
+          })
+          .catch((error) => {
+            console.error('Error fetching colors:', error);
+          });
+      }
+    },
+    setEffect() {
+      const formData = {
+        strip: this.selectedStrip,
+        color: this.selectedColor,
+        brightness: this.brightness,
+      };
+
+      axios
+        .post('http://localhost:3000/color', formData)
+        .then((response) => {})
+        .catch((error) => {
+          console.error(error);
         });
     },
-    seteffect() {
-      const formdata = {
-        strip: this.selectedStrip, 
-        color: this.selectedColor, 
-        brightness: this.brightness 
-      };
-      
-      axios.post('http://localhost:3000/color', formdata)
-        .then(response => {
-        })
-        .catch(error => {
-          console.error(error); // Als er een fout optreedt bij het verzoek
-        });
-    }
   },
   mounted() {
     this.fetchColors();
     setInterval(this.fetchColors, 1); // Fetch colors every second
-    // We gebruiken een watcher om de effecten automatisch bij te werken wanneer de gebruiker de instellingen wijzigt
-    this.$watch(() => [this.brightness, this.selectedColor, this.selectedStrip], () => {
-      this.seteffect();
-    });
-  }
+    this.$watch(
+      () => [this.brightness, this.selectedColor, this.selectedStrip],
+      () => {
+        this.setEffect();
+      }
+    );
+  },
 };
 </script>
