7c5977c414c643fd62339bb4dcb0e0aa750ff1eb
SimonStnn <simon.stijnen.23@gmail.com>
2024-02-28 19:15:33 +0100

    Add LED pixel component #62

---
+++ website/src/components/led-pixel.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { cn } from '@/lib/utils';
+
+type hexColor = string;
+const props = defineProps({
+  color: {
+    type: String as () => hexColor,
+    required: true,
+  },
+  class: {
+    type: String as () => string,
+    default: '',
+  },
+});
+</script>
+
+<template>
+    <div :class="cn('size-4', props.class)" :style="{ backgroundColor: props.color }"></div>
+</template>
