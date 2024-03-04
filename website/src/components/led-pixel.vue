5cc7a99715d78e050f28d1df4c2a35b5e8b6956f
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-04 11:33:00 +0100

    Set button text to copied when copying color hex close #73

--- website/src/components/led-pixel.vue
+++ website/src/components/led-pixel.vue
@@ -27,10 +27,39 @@ const props = defineProps({
       </TooltipTrigger>
       <TooltipContent class="space-x-1">
         <code class="bg-muted rounded py-[2px] px-1">{{ props.color }}</code>
-        <Button variant="secondary" class="py-[2px] px-1 h-5 text-xs" @click="() => copyToClipboard(props.color)">
-          Copy
+        <Button
+          variant="secondary"
+          class="py-[2px] px-1 h-5 text-xs"
+          @click="
+            () => {
+              console.log('copying', props.color);
+              setButtonText('Copied!', 1000);
+              copyToClipboard(props.color);
+            }
+          "
+        >
+          {{ copyButtonText }}
         </Button>
       </TooltipContent>
     </Tooltip>
   </TooltipProvider>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      copyButtonText: 'Copy',
+    };
+  },
+  methods: {
+    setButtonText(text: string, duration: number = 2000) {
+      const prevText = this.copyButtonText;
+      this.copyButtonText = text;
+      setTimeout(() => {
+        this.copyButtonText = prevText;
+      }, duration);
+    },
+  },
+};
+</script>
