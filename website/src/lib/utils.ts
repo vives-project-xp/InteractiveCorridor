4101bf99e34baf6a312761be5e425281b4402516
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-04 12:05:50 +0100

    Add throttle method to utils

--- website/src/lib/utils.ts
+++ website/src/lib/utils.ts
@@ -6,6 +6,32 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+let shouldWait = false;
+let waitingArgs: any[] | null = null;
+export function throttle(cb: Function, delay = 1000) {
+  const timeoutFunc = () => {
+    if (waitingArgs == null) {
+      shouldWait = false;
+    } else {
+      cb(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc, delay);
+    }
+  };
+
+  return ((...args: any[]) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+
+    cb(...args);
+    shouldWait = true;
+
+    setTimeout(timeoutFunc, delay);
+  })();
+}
+
 export function copyToClipboard(text: string) {
   navigator.clipboard.writeText(text);
 }
