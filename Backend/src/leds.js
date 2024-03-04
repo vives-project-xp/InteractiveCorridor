29ea63a032fd630e1308615eafbd34b84a75ba64
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-04 10:13:36 +0100

    Add comments

--- Backend/src/leds.js
+++ Backend/src/leds.js
@@ -9,10 +9,12 @@ async function fetchLedStrip(url) {
 }
 
 const getLeds = async (req, res) => {
+  // Send requests to all 16 LED strips
   const promises = [];
   for (let i = 1; i <= 16; i++) {
     promises.push(fetchLedStrip(`http://ic${i}.local/json/live`));
   }
+  // Wait for each response, then count the number of successful responses
   Promise.all(promises).then((responses) => {
     console.log(responses);
     const ledcount = responses.reduce((acc, response) => {
