ca111877737c25a5ae4a57c6046fcaa9b203c79a
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-11 10:59:23 +0100

    Cache ledstrip amount in server

--- Backend/src/leds.js
+++ Backend/src/leds.js
@@ -1,4 +1,4 @@
-async function fetchLedStrip(url, timeout = 2000) {
+async function fetchLedStrip(url, timeout = 5000) {
   try {
     const controller = new AbortController();
     const id = setTimeout(() => controller.abort(), timeout);
@@ -15,22 +15,36 @@ async function fetchLedStrip(url, timeout = 2000) {
   }
 }
 
-const getLeds = async (req, res) => {
+const discoverStrips = async () => {
+  console.log("Discovering LED strips");
+
   // Send requests to all 16 LED strips
   const promises = [];
   for (let i = 1; i <= 16; i++) {
     promises.push(fetchLedStrip(`http://ic${i}.local/json/live`));
   }
   // Wait for each response, then count the number of successful responses
-  Promise.all(promises).then((responses) => {
+  return await Promise.all(promises).then((responses) => {
     const ledcount = responses.reduce((acc, response) => {
       if (response.ok) {
         return acc + 1;
       }
       return acc;
     }, 0);
-    res.send(ledcount.toString());
+    console.log(`Discovered ${ledcount} LED strips`);
+    return ledcount;
   });
+}
+let ledstripCache;
+(async ()=>{
+  ledstripCache = await discoverStrips()
+  setInterval(async () => {
+    ledstripCache = await discoverStrips()
+  }, 60000);
+})()
+
+const getLeds = async (req, res) => {
+  res.send(ledstripCache?.toString()|| "0");
 };
 
 module.exports = { getLeds };
