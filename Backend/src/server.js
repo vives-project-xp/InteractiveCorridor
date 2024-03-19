bc1e10e397b7cddfedb5de0b64452ccded65d190
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-19 11:00:54 +0100

    Update server.js file with Swagger UI customization

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-const db = require('./dB');
+const db = require("./dB");
 const colors = require("./color");
 const effects = require("./effect");
 const leds = require("./leds");
@@ -14,22 +14,32 @@ const swaggerJSdoc = require("swagger-jsdoc");
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJSdoc({
-  definition: {
-    openapi: '3.1.0',
-    info: {
-      title: 'Interactive Corridorn API',
-      version: process.env.npm_package_version || '0.0.0',
-    },
-  },
-  apis: ['./**/swagger.yaml', './api/**/*.ts'],
-})));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(
+    swaggerJSdoc({
+      definition: {
+        openapi: "3.1.0",
+        info: {
+          title: "Interactive Corridor API",
+          version: process.env.npm_package_version || "0.0.0",
+        },
+      },
+      apis: ["./**/swagger.yaml", "./api/**/*.ts"],
+    }),
+    {
+      customCss: ".swagger-ui .topbar { display: none }",
+      customSiteTitle: "Interactive Corridor API Documentation",
+    }
+  )
+);
 
 app.get("/leds", leds.getLeds);
 
 app.post("/color", colors.setColor);
 app.post("/effect", effects.setEffect);
-app.get('/data', db.getDatabaseData);
+app.get("/data", db.getDatabaseData);
 
 app.get("/effect", effects.getEffect);
 
