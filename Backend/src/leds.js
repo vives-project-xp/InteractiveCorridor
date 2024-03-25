c2d6651355c79ce3968d97834c0bc52ed5463bf6
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-25 15:14:05 +0100

    Refactor getLeds and add postLeds function

--- Backend/src/leds.js
+++ Backend/src/leds.js
@@ -1,14 +1,13 @@
 const mqtt = require("./mqtt");
 const { ledstrips } = require("./ledstrips");
+const { hexToRgb } = require("./utils");
 
 const getLeds = async (req, res) => {
-  console.log("Getting LEDS");
   const strips = [];
 
   for (const strip of ledstrips) {
     const segments = [];
     for (const segment of strip.segments) {
-      console.log(segment);
       segments.push({
         start: segment.start,
         end: segment.end,
@@ -25,4 +24,25 @@ const getLeds = async (req, res) => {
   res.send(strips);
 };
 
-module.exports = { getLeds };
+const postLeds = async (req, res) => {
+  const strips = req.body;
+  for (const reqStrip of strips) {
+    const strip = ledstrips.find((s) => s.index === reqStrip.index);
+
+    if (!strip) continue;
+
+    for (let i = 0; i < reqStrip.segments.length; i++) {
+      const seg = strip.segments[i];
+      const reqColor = hexToRgb(reqStrip.segments[i].color);
+      if (reqColor !== seg.color) seg.setColor(reqColor);
+      if (reqStrip.segments[i].start !== seg.start)
+        seg.setStart(reqStrip.segments[i].start);
+      if (reqStrip.segments[i].end !== seg.end)
+        seg.setEnd(reqStrip.segments[i].end);
+    }
+  }
+
+  return getLeds(req, res); // return the updated ledstrips
+};
+
+module.exports = { getLeds, postLeds };
