c0db9e4b5e3ec9888fb46f60f5698837cd544cf8
SimonStnn <simon.stijnen.23@gmail.com>
2024-03-25 15:28:48 +0100

    Update LED strip rendering

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
-import { Skeleton } from '@/components/ui/skeleton'
+import { Skeleton } from '@/components/ui/skeleton';
 import ColorPicker from '@/components/color-picker.vue';
 import LedEffect from '@/components/led-effect.vue';
 import LedPixel from '@/components/led-pixel.vue';
@@ -18,40 +18,12 @@ export type Effect = {
 
 export type IncomingStrip = {
   index: number;
-  state: {
-    on: boolean;
-    bri: number;
-    seg: {
-      id: number;
-      start: number;
-      stop: number;
-      len: number;
-      grp: number;
-      spc: number;
-      of: number;
-      on: boolean;
-      frz: boolean;
-      bri: number;
-      cct: number;
-      set: number;
-      col: number[][];
-      fx: number;
-      sx: number;
-      ix: number;
-      pal: number;
-      c1: number;
-      c2: number;
-      c3: number;
-      sel: boolean;
-      rev: boolean;
-      mi: boolean;
-      o1: boolean;
-      o2: boolean;
-      o3: boolean;
-      si: number;
-      m12: number;
-    }[];
-  };
+  name: string;
+  segments: {
+    start: number;
+    end: number;
+    color: string;
+  }[];
 };
 </script>
 
@@ -124,36 +96,38 @@ export type IncomingStrip = {
       <template v-for="strip in strips" :key="strip.index">
         <div class="mb-5">
           <div>
-            <h3>LED-strip {{ strip.index }}</h3>
-            <div v-if="colors[strip.index]?.length > 0" class="flex flex-wrap gap-2">
-              <template v-for="(segment, barIndex) in strip.state.seg" :key="barIndex">
-                <div
-                  class="flex items-center rounded m-1 cursor-pointer"
-                  :class="{
-                    'shadow-[0px_0px_0px_5px_rgba(109,40,217,0.5)]':
-                      selectedStrips[strip.index]?.includes(barIndex),
-                  }"
-                  @click="
-                    () => {
-                      if (!selectedStrips[strip.index]) selectedStrips[strip.index] = [];
-                      const selectedStrip = selectedStrips[strip.index];
+            <h3>LED-strip {{ strip.name }}</h3>
+            <div
+              v-for="(segment, barIndex) in strip.segments"
+              :key="barIndex"
+              class="flex flex-wrap gap-2"
+            >
+              <div
+                class="flex items-center rounded m-1 cursor-pointer"
+                :class="{
+                  'shadow-[0px_0px_0px_5px_rgba(109,40,217,0.5)]':
+                    selectedStrips[strip.index]?.includes(barIndex),
+                }"
+                @click="
+                  () => {
+                    if (!selectedStrips[strip.index]) selectedStrips[strip.index] = [];
+                    const selectedStrip = selectedStrips[strip.index];
 
-                      if (selectedStrip.includes(barIndex)) {
-                        selectedStrip.splice(selectedStrip.indexOf(barIndex), 1);
-                      } else {
-                        selectedStrip.push(barIndex);
-                      }
+                    if (selectedStrip.includes(barIndex)) {
+                      selectedStrip.splice(selectedStrip.indexOf(barIndex), 1);
+                    } else {
+                      selectedStrip.push(barIndex);
                     }
-                  "
-                >
-                  <LedPixel
-                    v-for="(ledIndex, ledIndexInBar) in segment.len"
-                    :key="ledIndexInBar"
-                    class="first:rounded-l first:border-l last:rounded-r last:border-r border-y"
-                    :color="colors[strip.index][segment.start + ledIndex - 1] || '#000000'"
-                  ></LedPixel>
-                </div>
-              </template>
+                  }
+                "
+              >
+                <LedPixel
+                  v-for="(ledIndex, ledIndexInBar) in segment.end - segment.start + 1"
+                  :key="ledIndexInBar"
+                  class="first:rounded-l first:border-l last:rounded-r last:border-r border-y"
+                  :color="segment.color || '#000000'"
+                ></LedPixel>
+              </div>
             </div>
           </div>
         </div>
@@ -173,39 +147,18 @@ export default {
       brightness: 200,
       selectedColor: '#ff0000',
       selectedStrips: [] as number[][],
-      colors: [] as string[][],
       searching: false,
     };
   },
   methods: {
-    fetchColors() {
-      const updatedColors = [];
-      for (const strip of this.strips) {
-        updatedColors.push(strip.index);
-        axios
-          .get('http://ic' + Number(strip.index) + '.local/json/live')
-          .then((response) => {
-            const colorsObject = response.data.leds;
-            const colors = Object.values(colorsObject).map((color) => `#${color}`);
-            this.colors[strip.index] = colors;
-          })
-          .catch(() => {});
-      }
-
-      // Remove colors for LED strips that are no longer available
-      for (const stripIndex in this.colors) {
-        if (!updatedColors.includes(Number(stripIndex))) {
-          delete this.colors[stripIndex];
-        }
-      }
-    },
     fetchLeds() {
       this.searching = true;
       axios
         .get('http://localhost:3000/leds')
         .then(async (response) => {
           console.log('Got', response.data.length, 'LED strips from the server.');
-          this.strips = []; //response.data;
+          console.log('LED strips:', response.data);
+          this.strips = response.data;
           this.searching = false;
         })
         .catch((error) => {
@@ -245,9 +198,7 @@ export default {
   mounted() {
     this.fetchLeds();
     this.fetchEffects();
-    this.fetchColors();
-    setInterval(this.fetchColors, 100);
-    setInterval(this.fetchLeds, 5000);
+    setInterval(this.fetchLeds, 1000);
     this.$watch(
       () => [this.brightness, this.selectedColor, this.selectedStrips],
       () => {
