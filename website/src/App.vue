570ce4dfee2219fd5e331d98d169a196838e0925
SimonStnn <simon.stijnen.23@gmail.com>
2024-05-19 17:29:11 +0200

    Add github and docs links to website header #188

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { Button } from '@/components/ui/button';
-import { Moon, Sun } from 'lucide-vue-next';
+import { BookMarked, Github, Moon, Sun } from 'lucide-vue-next';
 </script>
 
 <template>
@@ -11,9 +11,31 @@ import { Moon, Sun } from 'lucide-vue-next';
     >
       Interactive Corridor
     </span>
-    <ul>
+    <ul class="flex gap-4">
       <li>
         <!-- <RouterLink to="/" :class="navItemClass">Home</RouterLink> -->
+      </li>
+      <li>
+        <a
+          href="https://github.com/vives-project-xp/InteractiveCorridor#readme"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="outline">
+            <Github />
+          </Button>
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vives-project-xp/InteractiveCorridor/tree/main/docs#readme"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="outline">
+            <BookMarked />
+          </Button>
+        </a>
       </li>
       <li>
         <Button variant="outline" @click="toggleDarkMode">
