f61ce5476d585e93ba24c0f54584976a99371883
ima_wizzard <aitor.vannevel@gmail.com>
2024-11-20 10:16:04 +0100

    hide navbar on tablet

--- website/src/App.vue
+++ website/src/App.vue
@@ -6,7 +6,7 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/comp
 </script>
 
 <template>
-  <nav class="flex justify-between items-center bg-secondary p-4 md:p-5 sm:hidden">
+  <nav class="flex justify-between items-center bg-secondary p-4 md:p-5 visible md:invisible">
     <span
       class="bg-gradient-to-r from-red-400 to-pink-400 bg-clip-text text-transparent text-xl sm:text-3xl font-bold"
     >
